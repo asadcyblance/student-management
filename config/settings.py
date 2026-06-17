e64e665f61079c4650188cf1cf8808ba59b6a0d7
AmishaChauhan0812 <amisha.cyblance@gmail.com>
2026-06-17 16:46:50 +0530

    Resolve image issues

--- config/settings.py
+++ config/settings.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     'dashboard',
 ]
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGOUT_REDIRECT_URL = 'login'
